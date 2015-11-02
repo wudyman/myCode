f3b0db129b5e4e3fe29860bd1a73c0844cd7b2a7
wudy <wudy.dong@gmail.com>
2015-11-02 21:04:32 +0800

    add AES dir

--- C/Unp/mqcreat1.c
+++ C/Unp/mqcreat1.c
@@ -1,7 +1,10 @@
 #include "sys/stat.h"
+#include "sys/types.h"
 #include "mqueue.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "unistd.h"
+#include "fcntl.h"
 #define FILE_MODE S_IRWXU|S_IRWXG|S_IRWXO
 int main(void)
 {
@@ -9,16 +12,21 @@ int main(void)
 	pid_t pid1;
 	char *msg="123";
 	char buff[20];
-	int flags=O_CREAT|O_RDWR|O_BLOCK;
+	int flags=O_CREAT|O_RDWR;
 	mq1=mq_open("/mqdxw1",flags,0600,NULL);
 	printf("mq1=%d\n",mq1);
 	if((pid1=fork())==0)
 	{
-		mq_send(mq1,msg,4,0);
+		mq_send(mq1,msg,4,4);
 		exit(0);
 	}
+	{
+  struct mq_attr mqAttr;
+  mq_getattr(mq1, &mqAttr);
+  printf("mqAttr.mq_msgsize=%ld\n",mqAttr.mq_msgsize);
 	sleep(3);
-	mq_receive(mq1,buff,4,0);
+	mq_receive(mq1,buff,mqAttr.mq_msgsize,NULL);
 	printf("receive is %s\n",buff);
+  }
 	return 0;
 }		
